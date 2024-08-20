76b84428c94b365eeef0018f936c587547cae35a
Marcos <marcosjbarroso82@gmail.com>
2024-08-20 13:14:07 +0000

    change

--- cooking_core/project/asgi.py
+++ cooking_core/project/asgi.py
@@ -1,6 +1,6 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cooking_core.project.settings')
